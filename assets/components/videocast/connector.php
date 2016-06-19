f836de7f3b61b7b13e4a3dc06e6ee83d809d21ba
Ivan Klimchuk <klimchuk@1pt.com>
2016-06-19 18:02:57 +0300

    Fixed require config in connector

--- assets/components/videocast/connector.php
+++ assets/components/videocast/connector.php
@@ -1,6 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../../../../../config.core.php';
+$configPath = __DIR__ . '/../../../';
+$configName = 'config.core.php';
+
+$config = file_exists($configPath . $configName)
+    ? $configPath . 'config.core.php'
+    : $configPath . '../../config.core.php';
+
+require_once $config;
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 
