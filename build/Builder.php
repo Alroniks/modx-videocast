631226fbe12bb25729651457325a7e1385e2995f
Ivan Klimchuk <klimchuk@1pt.com>
2016-07-11 15:05:56 +0300

    Extreamly improved release part of builder

--- build/Builder.php
+++ build/Builder.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/includes/Utils.php';
 class Builder
 {
     const PKG_NAME = 'videocast';
+    const PKG_NAME_HUMAN = 'VideoCast';
     const PKG_VERSION = '0.0.0';
     const PKG_RELEASE = 'pl';
 
@@ -369,21 +370,61 @@ class Builder
         return $this->modx->fromJSON($result);
     }
 
-    protected function publish($v)
+    /**
+     * @param $v - Release version
+     * @param $m - Release notes
+     */
+    protected function publish($v, $m)
     {
+        $m = escapeshellarg($m);
+
         // get latest release
         $release = $this->request('/repos/:owner/:repo/releases/latest');
 
         // set up new version
+        $realVersionNumber = $v-1;
         $version = $release['tag_name'];
         $version = explode('.', $version);
-        $version[$v-1]++;
+        $version[$realVersionNumber]++;
+        for ($i = $realVersionNumber + 1; $i < 3; $i++) {
+            $version[$i] = 0;
+        }
         $version = implode('.', $version);
+
+        $tags = [];
+        foreach (explode(PHP_EOL, shell_exec('git ls-remote --tags')) as $tag) {
+            if (!strlen($tag)) {
+                continue;
+            }
+
+            $tagInfo = explode('refs/tags/', $tag);
+            $tags[trim($tagInfo[1])] = trim($tagInfo[0]);
+        }
+
+        if (array_key_exists($release['tag_name'], $tags)) {
+            $file = __DIR__ . '/../meta/changelog.txt';
+
+            $hash = $tags[$release['tag_name']];
+            $cmd = "git log $hash..HEAD --pretty=format:'%h %s'";
+            $entries = shell_exec($cmd);
+
+            $package = self::PKG_NAME_HUMAN;
+            $date = (new DateTime)->format('M d, Y');
+
+            $info = ["$package $version-pl ($date)", '====================================', '', "    $m", '', $entries, '', ''];
+
+            $changeLog = file_get_contents($file);
+            $changeLog = join(PHP_EOL, $info) . $changeLog;
+            file_put_contents($file, $changeLog);
+
+            shell_exec("git commit -am '$m'");
+            shell_exec("git push origin master");
+        }
 
         // send new release
         $release = $this->request(
             '/repos/:owner/:repo/releases',
-            $this->modx->toJSON(['tag_name' => $version]),
+            $this->modx->toJSON(['tag_name' => $version, 'body' => $m]),
             ['Content-Type: application/json'],
             true
         );
@@ -412,58 +453,76 @@ class Builder
 
     public function __invoke()
     {
-        $package = self::PKG_NAME;
+        $package = self::PKG_NAME_HUMAN;
         $help = "
-            \033[32mBuilder \033[0mscript for package \033[33m{$package}
+            \e[32mRelease Builder \e[0mfor package \e[34m{$package}
             
-            \033[36mBy default script build and install package locally with version 0.0.0-pl
+            \e[92mScript allows to build and install package locally with version 0.0.0-pl and also push releases to github
              
-            \033[33mOptions: 
-            \033[32m--help          \033[0mDisplay this help message.
+            \e[33mOptions: 
+            \e[32m-h, --help          \e[0mDisplay this help message.
             
-            \033[33mUsage:
-              \033[0mphp Builder.php [options] [arguments]
+            \e[33mUsage:
+              \e[0mphp Builder.php [command] [options]
               
-            \033[33mAvailable commands:
-              \033[32mpublish       \033[0mBuild and release new version of package
+            \e[33mAvailable commands:
+              \e[32mbuild                           \t\t\e[0mBuilds current version of code to local package
+              \e[32mpublish \e[36mdescription   \e[0mBuilds new version of package and releases it to github
+                                            \t\t\e[0m<description> – Release notes \e[35m[required] \e[0m(\e[33mNote: \e[0muse quotes for description message)
               
-            \033[33mOptions:
-              \033[32m-1            \t\t\033[0mMajor version will be increased
-              \033[32m-2            \t\t\033[0mMinor version will be increased
-              \033[32m-3            \t\t\033[0mPatch version will be increased [default]\n";
+            \e[33mOptions:  
+              \e[32m-1            \t\t\e[0mMajor version will be increased
+              \e[32m-2            \t\t\e[0mMinor version will be increased
+              \e[32m-3            \t\t\e[0mPatch version will be increased [default]\n\n";
         $help = str_replace('            ', '', $help);
 
-        if (isset($this->argv[1]) && $this->argv[1] === '--help') {
+        if (isset($this->argv[1]) && ($this->argv[1] === '--help' || $this->argv[1] === '-h')) {
             echo $help;
 
             return;
         }
 
-        if (isset($this->argv[1]) && $this->argv[1] === 'publish') {
-            // check if all committed
-            $answer = shell_exec('git status');
-            if (strpos($answer, 'Changes not staged for commit') !== false) {
-                echo "\033[31mError: Changes aren't recorded into repository. You should commit and push all changes before publishing.\033[0m\n";
+        if (isset($this->argv[1])) {
+            // build package
+            if ($this->argv[1] === 'build') {
+                $this->build();
+                $this->install();
 
                 return;
             }
 
-            $v = str_replace('-', '', $this->argv[2] ?? '3');
+            // release package
+            if ($this->argv[1] === 'publish') {
+                // check if all committed
+                $answer = shell_exec('git status');
+                if (strpos($answer, 'Changes not staged for commit') !== false) {
+                    echo "\033[31mError: Changes aren't recorded into repository. You should commit and push all changes before publishing.\033[0m\n";
 
-            if (!in_array($v, [1, 2, 3])) {
-                echo "\033[31mError: Passed invalid version parameter\033[0m\n";
-                echo $help;
+                    return;
+                }
+
+                if (count($this->argv) < 3) {
+                    echo "\033[31mError: Description of release are missed. Use '--help' option to get details\033[0m\n";
+
+                    return;
+                }
+
+                $v = str_replace('-', '', $this->argv[3] ?? '3');
+                if (!in_array($v, [1, 2, 3])) {
+                    echo "\033[31mError: Passed invalid version parameter. Use '--help' option to get details\033[0m\n";
+
+                    return;
+                }
+
+                $this->publish($v, $this->argv[2]);
 
                 return;
             }
-
-            $this->publish($v);
-
-            return;
         }
 
-        $this->build();
-        $this->install();
+        echo $help;
+
+        return;
     }
 }
 
